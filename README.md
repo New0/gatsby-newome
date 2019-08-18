cef719ce3935d135b74e00e9ae13e82cf762d1d2
New0 <nico.figueira.mahe@gmail.com>
2019-08-18 09:32:53 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# gatsby-theme-publisher
+
+Based on [Scott Bolinger Gatsby theme Publisher](https://github.com/scottopolis/gatsby-theme-publisher)
+
+Inspired by [Jeff at Northstack](https://github.com/northstack/gatsby-wordpress-twenty-nineteen), [Zac Gordon](https://github.com/zgordon/tabor-gatsby-theme), and the [Tailwind Starter Theme](https://github.com/talensjr/gatsby-theme-tailwindcss).
---
+++ config.js
@@ -0,0 +1,21 @@
+/**
+ * I'm thinking we'll probably end up
+ * adding this to the .gitignore and have folks configure this on their own?
+ *
+ * ¯\_(ツ)_/¯
+ *
+ * @type {{wordPressUrl: string}}
+ */
+const config = {
+  title: `Nicolas Figueira`,
+  description: `Web Developer`,
+  author: `Nicolas Figueira`,
+  twitter: `nicofigueira`,
+  wordPressUrl: `https://newo.me`,
+  siteUrl: `https://newo.me`,
+  dynamicComments: true,
+  gaTrackingId: '',
+  mailChimpEndpoint: '',
+}
+
+module.exports = config
