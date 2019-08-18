8c123fcfc5140b086fb5a7b207c229641f2240cf
New0 <nico.figueira.mahe@gmail.com>
2019-08-18 10:25:22 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,11 @@ const Footer = () => {
     <footer className="site-footer bg-gray-900 p-8 w-full">
       <div className="max-w-6xl m-auto clearfix">
         <p className="text-sm text-gray-600 float-left">
-          © 2019 {author} | Built with Gatsby
+          © 2019 {author} | Built with Gatsby - WordPess content
         </p>
         <p className="float-right text-sm">
-          <a className="text-gray-600" href="https://twitter.com/scottbolinger">
+          <a className="text-gray-600" href="https://twitter.com/nicofigueira">
             Twitter
-          </a>{" "}
-          <a
-            className="text-gray-600"
-            href="https://theproductbusiness.com/podcast"
-          >
-            Podcast
           </a>
         </p>
       </div>
